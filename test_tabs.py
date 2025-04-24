b389f0b27ee6129e6cd150a1e7119dc11915046d
Daria Vasina <dash.vasina@gmail.com>
2025-04-24 23:23:13 +0300

    added test for main elements

--- test_tabs.py
+++ test_tabs.py
@@ -3,6 +3,27 @@ from utils.base_functions import *
 from utils.tabs import *
 from utils.search import *
 
+def test_main_elements(driver):
+    """Check that all main elements of the page are loaded"""
+    wait = get_wait(driver)
+    open_page(driver)
+
+    xpaths = [
+        "//div[contains(@class, 'header-logo') and @data-stat-type='logo']",
+
+        "//h1[@data-stat-type='menu' and text()='Погода']",
+        "//a[@href='/news/' and text()='Новости']",
+        "//a[@href='/maps/' and text()='Карты']",
+        "//a[@href='/soft/' and text()='Приложения']",
+
+        "//div[contains(@class, 'header-container') and contains(@class, 'wrap')]",
+        "//input[contains(@class, 'input js-input') and @placeholder='Поиск местоположения']",
+        "//div[contains(@class, 'widget') and contains(@class, 'cities-popular')]"
+    ]
+    for xpath in xpaths:
+        check_element_exists_by_xpath(xpath, driver)
+
+
 @pytest.mark.parametrize("tab_text,path", [
     ("Сейчас", CITY_PATH),
     ("По часам", CITY_PATH),
--- utils/base_functions.py
+++ utils/base_functions.py
@@ -1,4 +1,6 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 BASE_URL = "https://www.gismeteo.ru"
@@ -9,3 +11,10 @@ def open_page(driver, path: str = ""):
 
 def get_wait(driver, timeout: int = 10) -> WebDriverWait:
     return WebDriverWait(driver, timeout)
+
+def check_element_exists_by_xpath(xpath: str, driver, timeout: int = 10):
+    wait = WebDriverWait(driver, timeout)
+    try:
+        wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    except Exception:
+        raise AssertionError(f"Element not found by XPath: {xpath}")
